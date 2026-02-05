aecc4b279ab313733d62903f36b0b4861efcc026
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2026-02-05 14:39:47 -0600

    feat: test websocket setup on frontend

--- gnome-party-frontend/App.vue
+++ gnome-party-frontend/App.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const socket = new WebSocket("wss://nl4g15d5v5.execute-api.us-east-2.amazonaws.com/Prod");
+
+// Listen for messages
+socket.addEventListener("message", (event) => {
+  console.log("Message from server ", event.data);
+});
+
+</script>
 
 <template>
   <h1>You did it!</h1>
