46c9aa4a2dcc27d09b5fc9269a592982826a4ebc
laurenbissey <laurennbissey@gmail.com>
2026-04-29 12:14:24 -0500

    fix: add handling to continue button, start next encounter

--- gnome-party-frontend/host-view/scripts/ViewManager.ts
+++ gnome-party-frontend/host-view/scripts/ViewManager.ts
@@ -1292,6 +1292,14 @@ loadEncounter(gameState:any)
 				return;
 			}
 
+			if (buttonText === "Continue") {
+				this.socket.send(JSON.stringify({
+					route: "begin-combat-encounter",
+					GameSessionId: this.socketStore.gameSessionId
+				}))
+				return;
+			}
+
 			this.combatEndOverlayGroup?.destroy();
 			this.combatEndOverlayGroup = null;
 			this.uiLayer.draw();
